f8c4340a6f78b575f4628eaa602b5d6b05fd9ba0
dinhanhmarketing-lab <dinhanh.marketing@gmail.com>
2026-04-06 09:12:31 +0700

    app.jsx

--- app.jsx
+++ app.jsx
@@ -11,12 +11,13 @@ import {
 
 // --- 1. CẤU HÌNH FIREBASE (THAY BẰNG THÔNG TIN THẬT CỦA BẠN TẠI ĐÂY) ---
 const firebaseConfig = {
-  apiKey: "AIzaSy...", // Thay bằng API Key thật
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyC-HodBQeatCrJPLWcm96L6gLabVJvZKyw",
+  authDomain: "app-tuoi-cay-vvh.firebaseapp.com",
+  projectId: "app-tuoi-cay-vvh",
+  storageBucket: "app-tuoi-cay-vvh.firebasestorage.app",
+  messagingSenderId: "739614880511",
+  appId: "1:739614880511:web:53d229597ec9969b50d2ca",
+  measurementId: "G-343PBHZ7PB"
 };
 
 const app = initializeApp(firebaseConfig);
